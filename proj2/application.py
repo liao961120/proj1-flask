b13fd781163a38d6745df13ae3501b6a9006f7bc
liao <liao961120@gmail.com>
2019-06-30 00:22:15 +0800

    socket.io to create channels

--- proj2/application.py
+++ proj2/application.py
@@ -1,13 +1,32 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = dict()
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template('index.html')
+
+@socketio.on("create channel")
+def create_channel(data):
+    channel_id = data["channel-id"]
+    
+    # Check Create new empty channel if channel doesn't exist
+    if channel_id not in channels.keys():
+        channel_dict = {'id': channel_id, 'messages': [],}
+        channels[channel_id] = channel_dict
+
+    emit("update", channels, broadcast=True)
+
+@socketio.on('update channel')
+def update_channel():
+    emit('update', channels, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
